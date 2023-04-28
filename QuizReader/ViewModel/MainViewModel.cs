b2a6894714b40d5d98600643017b08a79d5dc262
Marcin <asssinowo90@gmail.com>
2023-04-29 00:08:51 +0200

    added counter and start/stop buttons

--- QuizReader/ViewModel/MainViewModel.cs
+++ QuizReader/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
@@ -10,12 +11,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace QuizReader.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        #region declarations
+        private string _currentTime;
+        private DispatcherTimer _timer;
+        private TimeSpan time;
+        public string CurrentTime { get {return this._currentTime;} set { _currentTime = value; OnPropertyChanged(); } }
+        private int counter;
         private bool _activated;
+        private bool _startEnabled;
+        private bool _stopEnabled;
         private string _answerAText;
         private string _answerBText;
         private string _answerCText;
@@ -27,14 +37,17 @@ namespace QuizReader.ViewModel
         public ICommand AnswerC { get; set; }
         public ICommand AnswerD { get; set; }
         public ICommand LoadQuiz { get; set; }
-        public bool activate { get { return _activated; } set { _activated = value; OnPropertyChanged(); } }
-        public List<string> listed { get { return _listed; } set { _listed = value; OnPropertyChanged(); } }
+        public ICommand Start { get; set; }
+        public ICommand Stop { get; set; }
         public string _pointSum { get; set; }
         public string CurrentAnswer { get; set; }
         public Score score { get; set; }
         public object clicked { get; set; }
         ListQuiz listQuiz { get; set; }
-        private int counter;
+        public bool startEnable { get { return _startEnabled; }set { _startEnabled = value;OnPropertyChanged(); } }
+        public bool stopEnable { get { return _stopEnabled; }set { _stopEnabled = value; OnPropertyChanged(); } }
+        public bool activate { get { return _activated; } set { _activated = value; OnPropertyChanged(); } }
+        public List<string> listed { get { return _listed; } set { _listed = value; OnPropertyChanged(); } }
         public string AnswerAText { get { return _answerAText; } set { _answerAText = value;  OnPropertyChanged(); } }
         public string AnswerBText { get { return _answerBText; }set { _answerBText = value; OnPropertyChanged(); } }
         public string AnswerCText { get { return _answerCText; }set { _answerCText = value; OnPropertyChanged(); } }
@@ -42,6 +55,7 @@ namespace QuizReader.ViewModel
         public string QuestionText { get { return _questionText; }set { _questionText = value; OnPropertyChanged(); } }
         public string PointSum { get { return _pointSum; } set { _pointSum= value; OnPropertyChanged(); } }
         public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion
         public MainViewModel() { 
             AnswerAText = "odp:A";
             AnswerBText = "odp:B";
@@ -49,36 +63,60 @@ namespace QuizReader.ViewModel
             AnswerDText = "odp:D";
             QuestionText = "Pytanie:";
             activate = false;
+            startEnable = false;
+            stopEnable =false;
             LoadQuiz = new RelayCommand(loadQuiz);
             AnswerA = new RelayCommand(ButtonA);
             AnswerB = new RelayCommand(ButtonB);
             AnswerC = new RelayCommand(ButtonC);
             AnswerD = new RelayCommand(ButtonD);
+            Start = new RelayCommand(StartB);
+            Stop = new RelayCommand(StopB);
             listQuiz = new ListQuiz();
+            CurrentTime = TimeSpan.Zero.ToString();
+            var time = TimeSpan.Zero;
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += timer_Tick;
         }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            time = time.Add(TimeSpan.FromSeconds(1));
+            CurrentTime = time.ToString(@"hh\:mm\:ss");
+        }
+
         private void OnPropertyChanged([CallerMemberName]string propertyName = null) {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         private void loadQuiz(object obj)
         {
             listQuiz.initialize();
-            score = new Score(0,listQuiz.QuizList.Count);
-            listed = listQuiz.QuizList.Select(x=>x.QuestName).ToList();
+
+            startEnable = true;
+
+        }
+        private void StartB(object obj)
+        {
+            _timer.Start();
+            startEnable = false;
+            stopEnable = true;
+            score = new Score(0, listQuiz.QuizList.Count);
+            listed = listQuiz.QuizList.Select(x => x.QuestName).ToList();
             if (listed.Count != 0)
             {
-
                 counter = 0;
                 activate = true;
                 var first = listQuiz.QuizList.First();
-                PointSum = score.ToString();
-                AnswerAText = first.answer.AnswerA;
-                AnswerBText = first.answer.AnswerB;
-                AnswerCText = first.answer.AnswerC;
-                AnswerDText = first.answer.AnswerD;
-                QuestionText = first.Question.Quest;
-                CurrentAnswer = first.answer.CorrectAnswer;
-                counter++;
+                updateView(first);
             }
+        }
+        private void StopB(object obj)
+        {
+            _timer.Stop();
+            startEnable = true;
+            stopEnable = false;
+            MessageBox.Show(score.ToString() + "\n" + "Twój czas: " + time);
+            updateView(null);
         }
         private void updateView(Quiz item)
         {
@@ -93,6 +131,9 @@ namespace QuizReader.ViewModel
                 CurrentAnswer = string.Empty;
                 counter = 0;
                 listed = new List<string>();
+                time = TimeSpan.Zero;
+                CurrentTime = time.ToString(@"hh\:mm\:ss");
+                _timer.Stop();
             }
             else
             {
@@ -108,123 +149,56 @@ namespace QuizReader.ViewModel
         }
         private void ButtonA(object obj)
         {
-            if (counter >= listQuiz.QuizList.Count)
-            {
-                if (CurrentAnswer.Equals("A"))
-                {
-                    score.addScore();
-                    MessageBox.Show(score.ToString());
-                    updateView(null);
-                    activate = false;
-                }
-                    else
-                {
-                    MessageBox.Show(PointSum.ToString());
-                    updateView(null);
-                    activate = false;
-                }
-            }
-            else
-            {
-                if (CurrentAnswer.Equals("A"))
-                {
-                    score.addScore();
-                    PointSum = score.ToString();
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-                }
-                else
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-            }
+            button_reaction("A");
         }
         private void ButtonB(object obj)
         {
-            if (counter >= listQuiz.QuizList.Count)
-            {
-                if (CurrentAnswer.Equals("B"))
-                {
-                    score.addScore();
-                    MessageBox.Show(score.ToString());
-                    updateView(null);
-                    activate = false;
-                }
-                else
-                {
-                    MessageBox.Show(PointSum.ToString());
-                    updateView(null);
-                    activate = false;
-                }
-            }
-            else
-            {
-                if (CurrentAnswer.Equals("B"))
-                {
-                    score.addScore();
-                    PointSum = score.ToString();
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-                }
-                else
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-            }
+            button_reaction("B");
         }
         private void ButtonC(object obj)
         {
-            if (counter >= listQuiz.QuizList.Count)
-            {
-                if (CurrentAnswer.Equals("C"))
-                {
-                    score.addScore();
-                    MessageBox.Show(score.ToString());
-                    updateView(null);
-                    activate = false;
-                }
-                else
-                {
-                    MessageBox.Show(PointSum.ToString());
-                    updateView(null);
-                    activate = false;
-                }
-            }
-            else
-            {
-                if (CurrentAnswer.Equals("C"))
-                {
-                    score.addScore();
-                    PointSum = score.ToString();
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-                }
-                else
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-            }
+            button_reaction("C");
         }
         private void ButtonD(object obj)
         {
-            if (counter >= listQuiz.QuizList.Count)
-            {
-                if (CurrentAnswer.Equals("D"))
+            button_reaction("D");
+        }
+        void button_reaction(string answer)
+        {
+                if (counter >= listQuiz.QuizList.Count)
                 {
-                    score.addScore();
-                    MessageBox.Show(score.ToString());
-                    updateView(null);
-                    activate = false;
+                    if (CurrentAnswer.Equals(answer))
+                    {
+                        score.addScore();
+                        MessageBox.Show(score.ToString() + "\n" + "Twój czas: " + time);
+                        updateView(null);
+                        activate = false;
+
+                        stopEnable = false;
+                        startEnable = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show(PointSum.ToString() + "\n" + "Twój czas: " + time);
+                        updateView(null);
+                        activate = false;
+
+                        stopEnable = false;
+                        startEnable = true;
+                    }
                 }
                 else
                 {
-                    MessageBox.Show(PointSum.ToString());
-                    updateView(null);
-                    activate = false;
+                    if (CurrentAnswer.Equals(answer))
+                    {
+                        score.addScore();
+                        PointSum = score.ToString();
+                        updateView(listQuiz.QuizList.ElementAt(counter));
+                    }
+                    else
+                        updateView(listQuiz.QuizList.ElementAt(counter));
                 }
-            }
-            else
-            {
-                if (CurrentAnswer.Equals("D"))
-                {
-                    score.addScore();
-                    PointSum = score.ToString();
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-                }
-                else
-                    updateView(listQuiz.QuizList.ElementAt(counter));
-            }
+            
         }
     }
 }
