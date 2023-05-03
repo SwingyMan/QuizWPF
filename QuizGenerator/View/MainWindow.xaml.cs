9ad34be7714b3d681604508d21ef12ea77abf3ab
Marcin <asssinowo90@gmail.com>
2023-05-03 21:11:05 +0200

    added mvvm model for generator

---
+++ QuizGenerator/View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using QuizGenerator.ViewModel;
+using System.Windows;
+
+namespace QuizGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ QuizGenerator/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public MainViewModel() { }
+    }
+}
